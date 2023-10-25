3d1cd864bec6d722096ee94cb106124bd4e982ad
VicenteCaraT <vit.cara@alumno.um.edu.ar>
2023-10-24 22:59:53 -0300

    adding different main model

--- main.py
+++ main.py
@@ -1,27 +1,17 @@
-from game.scrabbleGame import ScrabbleGame
+from game.scrabbleGame import (ScrabbleGame,)
 
 
 def main():
-    print("¡Bienvenido, a Scrabble!")
-    while True:
-        try: 
-            players_count = int(input("Ingrese cantidad de jugadores: "))
-            if players_count <= 1 or players_count > 4:
-                raise ValueError
-            break
-        except ValueError:
-            print("Valor invalido")
-    scrabble_game = ScrabbleGame(total_players=players_count)
-    print("Cantidad de jugadores: ",len(scrabble_game.players))
-    scrabble_game.next_turn()
-    print(f"Turno del jugador {scrabble_game.current_player.id}")
-    word = input("Ingrese palabra: ")
-    location_x = input("Ingrese posicion X: ")
-    location_y = input("Ingrese posicion Y: ")
-    location = (location_x, location_y)
-    orientation = input("Ingrese orientacion (V/H)")
-    scrabble_game.validate_word(word, location, orientation)
-
+    player_count = get_player_count()
+    game = ScrabbleGame(player_count)
+    while game.is_playing():
+        show_board(game.get_board())
+        show_player(*game.get_current_player())
+        word, coords, orientation = get_inputs()
+        try:
+            game.play(word, coords, orientation)
+        except Exception as e:
+            print(e)
 
 if __name__ == '__main__':
     main()
