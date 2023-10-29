d3f8dc5e55dd27a3674f0a842d90b451ac8d42c2
VicenteCaraT <vit.cara@alumno.um.edu.ar>
2023-10-29 19:58:13 -0300

    added all functions to validate words, exchanging tiles, and play

--- game/scrabbleGame.py
+++ game/scrabbleGame.py
@@ -19,24 +19,9 @@ class ScrabbleGame():
         self.bag_tiles = BagTiles()
         self.players:list[Player] = []
         for index in range(total_players):
-            self.players.append(Player(id=index))
+            self.players.append(Player(id=index, bag_tiles=self.bag_tiles))
         self.current_player = None
-        self.status = None
-
-    def game_status(self):
-        self.status = 'Playing...'
-    
-    def fist_round_set(self):
-        for player in self.players:
-            player.refill(7)
-        self.next_turn()
-    
-    def game_over(self):
-        self.status = 'Game Over'
-        if len(self.bag_tiles.total_tiles) == 0:
-            return True
-        else:
-            return False
+        self.round = 1
 
     def get_player_count():
         while True:
@@ -47,48 +32,102 @@ class ScrabbleGame():
             except Exception as e:
                 print('ingrese un numero por favor')
 
-        return player_count
+        return player_count   
     
-    def word_is_correct(self, word,location,orientation):
-        pass
+    
+    def is_playing(self):
+        if len(self.bag_tiles.total_tiles) > 0:
+            return True
+        else:
+            raise False
 
-    def fist_round_fist_word(self,word,location, orientation):
-        pass
-        
     def next_turn(self): 
-        if self.current_player == None:
+        self.round += 1
+        if self.current_player is None or self.current_player == self.players[-1]:
             self.current_player = self.players[0]
-        else:            
+        else:
             index = self.players.index(self.current_player) + 1
-            if index >= len(self.players):
-                self.current_player = self.players[0]
-            else:
-                self.current_player = self.players[index]
+            self.current_player = self.players[index]
+        return self.current_player, self.round 
+    
+    def exchange_tiles(self, old_tiles_indices):
+        player = self.current_player
+        if all(1 <= i <= 7 for i in old_tiles_indices):
+            old_tiles = []
+            for i in old_tiles_indices:
+                old_tiles.append(player.playertiles[i - 1])
+
+            new_tiles = self.bag_tiles.take(len(old_tiles_indices))
+            exchanged_tiles = []
+
+            for i in old_tiles_indices:
+                exchanged_tiles.append(player.playertiles[i - 1])
+                player.playertiles[i - 1] = new_tiles.pop(0)
+
+            # Devuelve las fichas originales al saco
+            self.bag_tiles.put(old_tiles)
+
+            return exchanged_tiles
+        else:
+            print("Invalid tile indices. Please enter numbers from 1 to 7.")
+            return []
+    
+    def exchange_all_tiles(self):
+        self.current_player
+        all_tile_indx=list(range(1,8))
+        exchanged_tiles = self.exchange_tiles(all_tile_indx)
+        return exchanged_tiles
+    
+
+    def round_set(self):
+        for player in self.players:
+            while len(player.playertiles) < 7:
+                remaining_tiles = 7 - len(player.playertiles)
+                new_tiles = self.bag_tiles.take(remaining_tiles)
+                player.playertiles.extend(new_tiles)
+    
+    def start_game(self):
+        return self.round_set()
 
     def validate_word(self, word, location, orientation): #FIXING
-        if not Player().has_letters(word):
-            raise InvalidWordNoLetters("No cuenta con las fichas para armar esta palabra")
+        if not self.current_player.has_letter(word):
+            raise InvalidWordNoLetters("You don't have the tiles to form this word")   
+        '''  
         if not is_in_dictionary(word):
-            raise InvalidWordException("Su palabra no existe en el diccionario")
+            raise InvalidWordException("Word, doesn't exist")
+            '''
         if not self.board.valid_word_in_board(word, location, orientation):
-            raise InvalidPlaceWordException("Su palabra excede el tablero")
-        if not self.board.validate_word_place_board(word, location, orientation):
-            raise InvalidPlaceWordException("Su palabra esta mal puesta en el tablero")
-                
-    def play(self,word,location,orientation):
-        self.validate_word(word, location, orientation)
-        words = self.board.put_word(word,location,orientation)
-        total = self.board.calculate_word_score(words)
-        self.players[self.current_player].score += total
-        self.next_turn()
-        
-        
-    def get_board():
+            raise InvalidPlaceWordException("Your word exceeds the Board")
+        if not self.board.valid_word_in_place(word, location, orientation):
+            raise InvalidPlaceWordException("No valid position in board")
+    
+    def validate_word_fist_round(self, word, location, orientation):
+        if not self.current_player.has_letter(word):
+            raise InvalidWordNoLetters("You don't have the tiles to form this word")
         '''
-        muestra el tablero
-        '''
-    def put_words():
-        '''
-        Modifica el estado del tablero con las palabras consideradas como correctas
-        '''
+        if not is_in_dictionary(word):
+            raise InvalidWordException("Word doesn't exist")
+            '''
+        if not self.board.valid_word_in_board(word, location, orientation):
+            raise InvalidPlaceWordException("Your word exceeds the Board")
+        self.board.put_first_word(word, location, orientation)
 
+    
+    def play_first_round(self, word, location, orientation):
+        if not self.validate_word_fist_round(word, location, orientation):
+            self.current_player.play_word(word)
+            self.board.put_word(word, location, orientation)
+
+    def play(self,word,location,orientation):
+        if not self.validate_word(word, location, orientation):
+            self.current_player.play_word(word)
+            self.board.put_word(word,location,orientation)
+        '''
+        total = self.board.calculate_word_score(words)
+        self.players[self.current_player.id].score += total
+        '''
+        return self.current_player.score
+
+    
+    def get_current_player(self) -> Player:
+        return self.current_player
--- test/test_scrabblegame.py
+++ test/test_scrabblegame.py
@@ -30,19 +30,79 @@ class TestScrabblePlayers(unittest.TestCase):
         scrabble_game.current_player = scrabble_game.players[2]
         scrabble_game.next_turn()
         assert scrabble_game.current_player == scrabble_game.players[0]
-    
-    def test_first_round_set(self):
+
+    def test_next_turn_round(self):
+        # Arrange
+        total_players = 3
+        scrabble_game = ScrabbleGame(total_players=2)
+
+        # Act
+        current_player, round_number = scrabble_game.next_turn()
+
+        # Assert
+        expected_round_number = 2  # Se espera que la ronda se incremente a 2 después de la primera llamada a next_turn
+        self.assertEqual(round_number, expected_round_number, "La ronda no se incrementa correctamente.")
+
+        # Act nuevamente para verificar si la ronda sigue aumentando
+        current_player, round_number = scrabble_game.next_turn()
+
+        # Assert
+        expected_round_number = 3  # Se espera que la ronda se incremente a 3 después de la segunda llamada a next_turn
+        self.assertEqual(round_number, expected_round_number, "La ronda no se incrementa correctamente en el segundo turno.")
+'''
+    def test_get_current_player(self):
+        # Crear un juego con 3 jugadores
         scrabble_game = ScrabbleGame(total_players=3)
-        scrabble_game.fist_round_set()
-        for player in scrabble_game.players:
-            self.assertEqual(len(player.playertiles), 7)
-    
-    def test_game_over(self):
-        scrable_game = ScrabbleGame(total_players=2)
-        scrable_game.bag_tiles.total_tiles = []       
-        self.assertTrue(scrable_game.game_over())
-        scrable_game.bag_tiles.total_tiles = [
-            Tile('A', 1),
-            Tile('B', 1)
-        ]
-        self.assertFalse(scrable_game.game_over())
+
+        # Verificar que al principio el jugador actual es el jugador 0
+        scrabble_game.get_current_player()
+        self.assertEqual(scrabble_game.current_player.id, None)
+
+        # Avanzar al siguiente turno
+        scrabble_game.next_turn()
+
+        # Verificar que ahora el jugador actual es el jugador 1
+        scrabble_game.get_current_player()
+        self.assertEqual(scrabble_game.current_player.id, 0)
+
+        # Avanzar al siguiente turno
+        scrabble_game.next_turn()
+
+        # Verificar que ahora el jugador actual es el jugador 2
+        scrabble_game.get_current_player()
+        self.assertEqual(scrabble_game.current_player.id, 1)
+
+        # Avanzar al siguiente turno (vuelve al primer jugador)
+        scrabble_game.next_turn()
+
+        # Verificar que ahora el jugador actual es nuevamente el jugador 0
+        scrabble_game.get_current_player()
+        self.assertEqual(scrabble_game.current_player.id, 0)
+        '''
+'''
+    def test_exchange_tiles(self):
+        scrabble_game = ScrabbleGame(total_players=2)
+        scrabble_game.next_turn()
+        initial_tiles = scrabble_game.current_player.playertiles = [
+            Tile(letter='A', value=1),
+            Tile(letter='B', value=1),
+            Tile(letter='C', value=1),
+            Tile(letter='D', value=1),
+            Tile(letter='E', value=1),
+            Tile(letter='F', value=1)
+        ]  # Configurar las fichas del jugador
+
+        # Índices de las fichas que se intercambiarán
+        tiles_to_exchange_indices = [1, 3, 5]  # Por ejemplo, intercambiar las fichas en las posiciones 1, 3 y 5
+
+        # Intercambiar las fichas y obtener las fichas intercambiadas
+        exchanged_tiles = scrabble_game.exchange_tiles(tiles_to_exchange_indices)
+
+        # Verificar que las fichas intercambiadas estén en la bolsa de fichas
+        for tile in exchanged_tiles:
+            self.assertIn(tile, scrabble_game.bag_tiles.total_tiles)
+
+        # Verificar que las fichas originales del jugador estén de vuelta en la bolsa de fichas
+        for index in tiles_to_exchange_indices:
+            self.assertNotIn(initial_tiles[index - 1], scrabble_game.current_player.playertiles)
+            '''
